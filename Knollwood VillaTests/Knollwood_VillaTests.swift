f8e07a1af056e2fedb7d25ee2ff1ad1d09addb2d
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-09 17:11:25 -0400

    Added Project to GitHub

---
+++ Knollwood VillaTests/Knollwood_VillaTests.swift
@@ -0,0 +1,34 @@
+//
+//  Knollwood_VillaTests.swift
+//  Knollwood VillaTests
+//
+//  Created by Flavius on 5/9/19.
+//  Copyright © 2019 Flavius. All rights reserved.
+//
+
+import XCTest
+@testable import Knollwood_Villa
+
+class Knollwood_VillaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
