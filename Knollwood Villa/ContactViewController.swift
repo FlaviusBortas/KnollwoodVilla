e3b8f2b4ea73095bdab93397e4bcf766c0b73048
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-13 17:40:32 -0400

    Created Contact Vc

---
+++ Knollwood Villa/ContactViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ContactView.swift
+//  Knollwood Villa
+//
+//  Created by Flavius on 5/13/19.
+//  Copyright © 2019 Flavius. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
