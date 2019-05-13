41666840fe2d24e4c99b147b00a346da8da13a66
FlaviusBortas <flaviusbortas@gmail.com>
2019-05-13 17:37:10 -0400

    Added segue identifiers in enum

---
+++ Knollwood Villa/SegueIdentifiers.swift
@@ -0,0 +1,18 @@
+//
+//  SegueIdentifiers.swift
+//  Knollwood Villa
+//
+//  Created by Flavius on 5/13/19.
+//  Copyright © 2019 Flavius. All rights reserved.
+//
+
+import Foundation
+
+enum Tabs: String {
+    
+    case aboutUs = "AboutUs"
+    case gallery = "Gallery"
+    case news = "News"
+    case contact = "Contact"
+    
+}
