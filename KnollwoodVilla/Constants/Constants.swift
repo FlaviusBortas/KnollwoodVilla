b306e37c4935bf993d54067f60592c509305ddf8
FlaviusBortas <flaviusbortas@gmail.com>
2019-08-24 14:31:46 -0400

    UIImage Extention
    moved KVTabBarIconImages to be an extention of UIImage

--- KnollwoodVilla/Constants/Constants.swift
+++ KnollwoodVilla/Constants/Constants.swift
@@ -25,36 +25,4 @@ class Constants {
     /* Should this belong here, computed properties are var objcets, while a constants file should contain only let variables or
         is this legal within the purpose of having a constants folder.
     */
-    
-    static var kvGalleryTabBarIcon: UIImage {
-        if let galleryIcon = UIImage(named: "GalleryIcon") {
-            return galleryIcon
-        }
-        
-       return UIImage()
-    }
-    
-    static var kvContactTabBarIcon: UIImage {
-        if let galleryIcon = UIImage(named: "ContactIcon") {
-            return galleryIcon
-        }
-        
-        return UIImage()
-    }
-    
-    static var kvNewsTabBarIcon: UIImage {
-        if let galleryIcon = UIImage(named: "NewsIcon") {
-            return galleryIcon
-        }
-        
-        return UIImage()
-    }
-    
-    static var kvHomeTabBarIcon: UIImage {
-        if let homeIcon = UIImage(named: "HomeIcon") {
-            return homeIcon
-        }
-        
-        return UIImage()
-    }
 }
--- KnollwoodVilla/Controllers/KVTabBarController.swift
+++ KnollwoodVilla/Controllers/KVTabBarController.swift
@@ -20,19 +20,19 @@ class KVTabBarController: UITabBarController {
 
     func setupTabBar() {
         let homeCollectionViewController = HomeCollectionViewController()
-        let homeIcon = Constants.kvHomeTabBarIcon
+        let homeIcon = UIImage.homeTabBarIcon
         homeCollectionViewController.tabBarItem = UITabBarItem(title: Constants.homeTabString, image: homeIcon, tag: 0)
         
         let galleryCollectionViewController = GalleryCollectionViewController()
-        let galleryIcon = Constants.kvGalleryTabBarIcon
+        let galleryIcon = UIImage.galleryTabBarIcon
         galleryCollectionViewController.tabBarItem = UITabBarItem(title: Constants.galleryTabString, image: galleryIcon, tag: 1)
         
         let newsTableViewController = NewsTableViewController()
-        let newsIcon = Constants.kvNewsTabBarIcon
+        let newsIcon = UIImage.newsTabBarIcon
         newsTableViewController.tabBarItem = UITabBarItem(title: Constants.newsTabString, image: newsIcon, tag: 2)
         
         let contactViewController = ContactViewController()
-        let contactIcon = Constants.kvContactTabBarIcon
+        let contactIcon = UIImage.contactTabBarIcon
         contactViewController.tabBarItem = UITabBarItem(title: Constants.contactTabString, image: contactIcon, tag: 3)
         
         // Changes the image of the slected item once clicked
